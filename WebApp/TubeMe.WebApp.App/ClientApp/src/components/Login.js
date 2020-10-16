f6d58090c4a296822614ba17c75962dd674da5dc
MomchilAngelov0040 <MomchilAngelov0040@gmail.com>
2020-10-16 18:59:50 +0300

    Initial commit

---
+++ WebApp/TubeMe.WebApp.App/ClientApp/src/components/Login.js
@@ -0,0 +1,16 @@
+﻿import React, { Component } from 'react';
+
+export class Login extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        Hello login
+      </div>
+    );
+  }
+}
