fc07127d10affbd93caba073b1088bb1c9b74918
MomchilAngelov0040 <MomchilAngelov0040@gmail.com>
2020-10-20 10:26:21 +0300

    Data pass tests

--- WebApp/TubeMe.WebApp.App/ClientApp/src/components/Home.js
+++ WebApp/TubeMe.WebApp.App/ClientApp/src/components/Home.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
+import Video from './Video'
 
 export class Home extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      email: ''
+      videos: [
+        {
+          id: 1,
+          imgUrl: "https://images.immediate.co.uk/production/volatile/sites/30/2017/01/Bananas-218094b-scaled.jpg?quality=90&resize=960%2C872",
+          number: 10
+        },
+        {
+          id: 2,
+          imgUrl: "https://upload.wikimedia.org/wikipedia/commons/8/8a/Banana-Single.jpg",
+          number: fff20
+        }
+      ]
     };
   }
 
-  componentDidMount() {
-    this.setState({
-      email: localStorage.getItem("Email")
-    })
+  renderVideos = () => {
+    return this.state.videos.map(video =>
+      <Video key={video.id} imageUrl={video.imgUrl} number={video.number} selected />
+    )
   }
 
   render() {
     return (
-      <div>
-        <h1>{this.state.email}</h1>
-      </div>
+      <React.Fragment>
+        {this.renderVideos()}
+      </React.Fragment>
     );
   }
 }
---
+++ WebApp/TubeMe.WebApp.App/ClientApp/src/components/Video.js
@@ -0,0 +1,26 @@
+﻿import React, { Component } from 'react';
+
+export default class Video extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      imageUrl: this.props.imageUrl,
+      number: this.props.number
+    }
+  }
+
+  render() {
+    console.log(this.props)
+    return (
+      <div className="card" style={{ width: '18rem' }}>
+        <img className="card-img-top" src={this.state.imageUrl} alt="Card cap" />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">{this.state.number}</p>
+          <a href="/#" className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    )
+  }
+}
